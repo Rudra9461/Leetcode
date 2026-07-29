2b34a8f1554152fde50d36e0236c67d375186df9
RUDRA PRATAP SINGH <pratapsinghr782@gmail.com>
2026-07-29 11:47:26 +0530

    Time: 443 ms (14.95%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -3,4 +3,5 @@ select query_name ,
 round(avg(rating/position),2) as quality ,
 round(avg(CASE when rating<3 then 1 else 0 end )*100.0 ,2) as poor_query_percentage 
 from Queries
+where query_name is not null
 group by query_name ;
