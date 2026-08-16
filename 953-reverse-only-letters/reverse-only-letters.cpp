7ca34501d306b824c5be29cb227abb4c4e7cdf0d
RUDRA PRATAP SINGH <pratapsinghr782@gmail.com>
2026-08-16 17:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (55.57%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int n=s.size();
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            if(!isalpha(s[l])){
+                l++;
+            }
+            else if(!isalpha(s[r])){
+                r--;
+            } 
+            else{
+                swap(s[l],s[r]);
+                l++;
+                r--;
+            }           
+        }
+        return s;
+    }
+};
