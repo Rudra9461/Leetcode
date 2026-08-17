98071ea0309e6c57f0a99f64132190f67a2709d8
RUDRA PRATAP SINGH <pratapsinghr782@gmail.com>
2026-08-17 12:40:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (29.13%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,43 @@
+// i have extra space as i created a string space named words
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stringstream ss(s);
+//         string word;
+//         vector<string>words;
+//         while(ss>>word){
+//             reverse(word.begin(),word.end());
+//             words.push_back(word);
+//         }
+//         string ans="";
+//         for(int i=0;i<words.size();i++){
+//             if(i>0){
+//                 ans+=" ";
+//             }
+//             ans+=words[i];
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     string reverseWords(string s) {
         stringstream ss(s);
         string word;
-        vector<string>words;
-        while(ss>>word){
-            reverse(word.begin(),word.end());
-            words.push_back(word);
-        }
-        string ans="";
-        for(int i=0;i<words.size();i++){
-            if(i>0){
-                ans+=" ";
+        string ans = "";
+
+        while (ss >> word) {
+            reverse(word.begin(), word.end());
+
+            if (!ans.empty()) {
+                ans += " ";
             }
-            ans+=words[i];
+
+            ans += word;
         }
+
         return ans;
     }
 };
