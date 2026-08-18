89af5ef408aa85ab7e8c15687d2db9807d441efc
RUDRA PRATAP SINGH <pratapsinghr782@gmail.com>
2026-08-18 13:33:24 +0530

    Time: 6 ms (64.29%) | Memory: 14.5 MB (97.35%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,18 +1,41 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         int n=s.size();
+//         unordered_map<char,int>mp;
+
+//         for(int i=0;i<n;i++){
+//             mp[s[i]]++;
+//         }
+
+//         for(int i=0;i<n;i++){
+//             if(mp[s[i]]==1){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+// using frequency array 
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int n=s.size();
-        unordered_map<char,int>mp;
+        int freq[26] = {0};
 
-        for(int i=0;i<n;i++){
-            mp[s[i]]++;
+        // Count frequency
+        for(int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
         }
 
-        for(int i=0;i<n;i++){
-            if(mp[s[i]]==1){
+        // Find first unique
+        for(int i = 0; i < s.size(); i++) {
+            if(freq[s[i] - 'a'] == 1) {
                 return i;
             }
         }
+
         return -1;
     }
 };
